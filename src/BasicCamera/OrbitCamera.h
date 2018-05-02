529513a1b1a9ef741ada70fcca4176396da7b60b
dormon <imilet@fit.vutbr.cz>
2018-05-02 20:04:20 +0200

    Initial commit

---
+++ src/BasicCamera/OrbitCamera.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_access.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <BasicCamera/Camera.h>
+#include <BasicCamera/Fwd.h>
+#include <BasicCamera/basiccamera_export.h>
+
+class BASICCAMERA_EXPORT basicCamera::OrbitCamera : public CameraTransform {
+ public:
+  OrbitCamera(float distance = 1);
+  virtual ~OrbitCamera();
+  virtual glm::mat4 getView();
+  glm::mat4         getRotation();
+  float             getXAngle() const;
+  float             getYAngle() const;
+  float             getDistance() const;
+  glm::vec3         getFocus() const;
+  void              setXAngle(float value);
+  void              setYAngle(float value);
+  void              setAngles(glm::vec2 const& angles);
+  void              setDistance(float value);
+  void              setFocus(glm::vec3 const& value);
+  void              addXPosition(float dx);
+  void              addYPosition(float dy);
+  void              addXYPosition(glm::vec2 const& value);
+  void              addXAngle(float value);
+  void              addYAngle(float value);
+  void              addAngles(glm::vec2 const& value);
+  void              addDistance(float value);
+
+ protected:
+  float     _xAngle;
+  float     _yAngle;
+  float     _distance;
+  bool      _recompute;
+  glm::vec3 _position;
+  glm::mat4 _view;
+  glm::mat4 _rotation;
+  void      _compute();
+};
---
+++ src/BasicCamera/PerspectiveCamera.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <BasicCamera/Camera.h>
+#include <BasicCamera/Fwd.h>
+#include <BasicCamera/basiccamera_export.h>
+
+class BASICCAMERA_EXPORT basicCamera::PerspectiveCamera
+    : public CameraProjection {
+ public:
+  PerspectiveCamera(float fovy   = glm::radians(90.f),
+                    float aspect = 1024.f / 768.f,
+                    float near   = 0.1f,
+                    float far    = 1000.f);
+  virtual ~PerspectiveCamera();
+  virtual glm::mat4 getProjection();
+  float             getNear() const;
+  float             getFar() const;
+  float             getFovy() const;
+  float             getAspect() const;
+  void              setNear(float value);
+  void              setFar(float value);
+  void              setFovy(float value);
+  void              setAspect(float value);
+
+ protected:
+  float     _near;
+  float     _far;
+  float     _fovy;
+  float     _aspect;
+  glm::mat4 _projection;
+  bool      _recompute;
+  void      _compute();
+};
